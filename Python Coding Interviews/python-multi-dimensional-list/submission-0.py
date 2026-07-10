453b65db076bd655983491dce987401eef2ee813
Tschosch <93154451+Tschosch7@users.noreply.github.com>
2026-07-10 21:12:44 +0200

    Add: python-multi-dimensional-list - submission-0

---
+++ Python Coding Interviews/python-multi-dimensional-list/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def find_max_in_each_list(nested_arr: List[List[int]]) -> List[int]:
+    res = []
+    for arr in nested_arr:
+        maximum = arr[0]
+        for val in arr:
+            maximum = max(val, maximum)
+        res.append(maximum)
+    return res
+
+
+# do not modify below this line
+print(find_max_in_each_list([[1, 2], [3, 4, 2]]))
+print(find_max_in_each_list([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+print(find_max_in_each_list([[5, 6, 2, 8], [9], [9, 10], [11, 10, 11]]))
